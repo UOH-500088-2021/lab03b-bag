461b10fbb6ced43a4218c1784406904f0e7a0b8f
GERGO CSONKA <G.CSONKA-2018@hull.ac.uk>
2021-10-31 20:19:23 +0000

    Test case is writen for the calls in January 2015

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace DataProcessor
 {
@@ -28,36 +30,45 @@ namespace DataProcessor
         public int NumberOfForces { get; set; }
         public string HighestAbandonmentRateName { get; set; }
 
+        public List<string> RankedListJan { get; set; }
+
         public void ReadData()
         {
             double highestAbandonmentRate = 0;
             using (StreamReader reader = new StreamReader(FileName))
             {
+                RankedListJan = new List<string>();
                 var totalCalls = 0;
-                var line = reader.ReadLine(); 
+                var line = reader.ReadLine();
+                Dictionary<string, double> ForcesWithAnsTime = new Dictionary<string, double>();
                 while (!reader.EndOfStream)
                 {
                     NumberOfForces++;
                     line = reader.ReadLine();
-                    var columns = line.Split(",");  
+                    var columns = line.Split(",");
                     int.TryParse(columns[1], out int num);
                     totalCalls += num;
 
                     highestAbandonmentRate = SetAbandonmentRate(columns, highestAbandonmentRate);
+
                 }
+
+
                 AverageTotalCalls = totalCalls / NumberOfForces;
             }
         }
 
+
         private double SetAbandonmentRate(string[] columns, double highestAbandonmentRate)
         {
-            double startCol = 27;
-            double endCol = 39;
+            int startCol = 27;
+            int endCol = 39;
             
             double totalAbandonment = 0;
-            for (int i = (int)startCol; i < endCol; i++)
+            for (int i = startCol; i < endCol; i++)
             {
-                totalAbandonment += double.Parse(columns[i]);
+                double.TryParse(columns[i], out double num);
+                totalAbandonment += num;
             }
 
             if (totalAbandonment > highestAbandonmentRate)
--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -1,5 +1,7 @@
 using DataProcessor;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UnitTests
 {
@@ -36,6 +38,20 @@ namespace UnitTests
             reader.ReadData();
             var result = reader.HighestAbandonmentRateName;
             Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void RankedListJan()
+        {
+            var reader = new DataReader();
+            reader.FileName = "police_101_call_test_data.csv";
+            List<string> expected = new List<string>{ "Cumbria Constabulary", "Cambridgeshire Constabulary", "City of London Police", "Cleveland Police", "Cheshire Constabulary", "Bedfordshire Police", "British Transport Police", "Avon and Somerset Constabulary" };
+            reader.ReadData();
+            var result = reader.RankedListJan;
+            for (int i =0;i<expected.Count();i++) 
+            {
+                Assert.AreEqual(expected[i],result[i]);
+            }
         }
     }
 }
