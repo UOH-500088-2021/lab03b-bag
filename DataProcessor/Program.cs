3fe998749c8acae89468595ec2d997adc5c685e1
ElongMusk <71707464+ElongMusk@users.noreply.github.com>
2021-10-31 17:26:09 +0000

    Refactored code into an adtioanl method called SetAbandonmentRate().

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -31,43 +31,46 @@ namespace DataProcessor
         public void ReadData()
         {
             var totalCallsColumn = 2;
-            double startCol = 27;
-            double endCol = 39;
-            double totalAbandonment = 0;
             double highestAbandonmentRate = 0;
 
             using (StreamReader reader = new StreamReader(FileName))
             {
                 var totalCalls = 0;
                 var line = reader.ReadLine(); // Read the header line, which doesn't contain data
-                string forecName = "";
                 while (!reader.EndOfStream)
                 {
                     NumberOfForces++;
                     line = reader.ReadLine();
                     var columns = line.Split(",");  // What if there's a comma inside a data column?
-                    
+
                     int.TryParse(columns[1], out int num);
                     totalCalls += num;
 
-                    for (int i = (int)startCol; i< endCol; i++)
-                    {
-                        totalAbandonment += double.Parse(columns[i]);
-                    }
-
-                    if (totalAbandonment > highestAbandonmentRate) 
-                    {
-                        forecName = columns[0];
-                        HighestAbandonmentRate = forecName;
-                        highestAbandonmentRate = totalAbandonment;
-                    }
-
-                    totalAbandonment = 0;
+                    highestAbandonmentRate = SetAbandonmentRate(columns, highestAbandonmentRate);
                 }
                 AverageTotalCalls = totalCalls / NumberOfForces;
             }
         }
-      
+
+        private double SetAbandonmentRate(string[] columns, double highestAbandonmentRate)
+        {
+            double startCol = 27;
+            double endCol = 39;
+            
+            double totalAbandonment = 0;
+            for (int i = (int)startCol; i < endCol; i++)
+            {
+                totalAbandonment += double.Parse(columns[i]);
+            }
+
+            if (totalAbandonment > highestAbandonmentRate)
+            {
+                highestAbandonmentRate = totalAbandonment;
+                HighestAbandonmentRate = columns[0];
+            }
+
+            return highestAbandonmentRate;
+        }
     }
         
 }
